c0e2022e020152f20f31e961583c7e60c43a2ab7
AqilSidek <aq493299@dal.ca>
2019-09-20 13:54:22 -0300

    Updates

---
+++ precache-manifest.f8cad8c345cf2172dcf74c81e643f41b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "016a619eaa60f39d0c582a06660219d6",
+    "url": "aqilsidek.github.io/TopView-Bike-Rental/index.html"
+  },
+  {
+    "revision": "2211529df77e51eb8d1b",
+    "url": "aqilsidek.github.io/TopView-Bike-Rental/static/css/main.6e0f08c4.chunk.css"
+  },
+  {
+    "revision": "8fc1d115e3df01a01480",
+    "url": "aqilsidek.github.io/TopView-Bike-Rental/static/js/2.678ec769.chunk.js"
+  },
+  {
+    "revision": "2211529df77e51eb8d1b",
+    "url": "aqilsidek.github.io/TopView-Bike-Rental/static/js/main.ca20371c.chunk.js"
+  },
+  {
+    "revision": "134785dc1fb8fd41f041",
+    "url": "aqilsidek.github.io/TopView-Bike-Rental/static/js/runtime~main.9425b7e1.js"
+  },
+  {
+    "revision": "08183632fc2d250e7616acb5d932413b",
+    "url": "aqilsidek.github.io/TopView-Bike-Rental/static/media/mike-c-valdivia-kZokA2VTKn4-unsplash.08183632.jpg"
+  }
+]);
